71cfad63e963ead20573fe8ad532c7a004b79c1c
iGus12 <ag.varas@duocuc.cl>
2026-09-17 13:48:14 -0300

    Add files via upload

---
+++ src/main/java/com/digitalfix/catalog/MsDigitalfixCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.digitalfix.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDigitalfixCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDigitalfixCatalogApplication.class, args);
+	}
+
+}
